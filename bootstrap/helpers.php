c342b65229ecf21bed18031f9f3fcdffd930d434
yangjiekang <yangjiekang@chinac.com>
2018-11-08 10:44:33 +0800

    前台基本布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,7 +5,7 @@
  * DateTime: 2018-11-07 16:04
  * Email: 121491162@qq.com
  */
-function test()
+function route_class()
 {
-    return "hello";
+    return str_replace('.', '-', Route::currentRouteName());
 }
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+
+        </p>
+
+        <p class="pull-right"><a href="mailto:121491162@qq.com">联系我们</a></p>
+    </div>
+</footer>
