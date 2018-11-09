dbca0317d9cfb3df023ba187d9e1ef88a2eb50d8
yangjiekang <yangjiekang@chinac.com>
2018-11-09 17:04:51 +0800

    后台管理新闻

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,20 +13,9 @@ Route::group([
 
     $router->get('/', 'HomeController@index');
 
-    $router->get('products/create', 'ProductsController@create');
-    $router->get('products', 'ProductsController@index');
-    $router->post('products', 'ProductsController@store');
-    $router->get('products/{id}/edit', 'ProductsController@edit');
-    $router->put('products/{id}', 'ProductsController@update');
-    $router->delete('products/{id}', 'ProductsController@destroy');
-
-    $router->get('articles/create', 'ArticlesController@create');
-    $router->get('articles', 'ArticlesController@index');
-    $router->post('articles', 'ArticlesController@store');
-    $router->get('articles/{id}/edit', 'ArticlesController@edit');
-    $router->put('articles/{id}', 'ArticlesController@update');
-    $router->delete('articles/{id}', 'ArticlesController@destroy');
-
+    $router->resource('products', 'ProductsController');
+    $router->resource('articles', 'ArticlesController');
     $router->resource('cases', 'CasesController');
+    $router->resource('news', 'NewsController');
 
 });
